90bb5683c9ad8f8a271d2f7fe35eb4306a990f6a
Ailsa <liuyi_61@163.com>
2018-07-23 16:31:06 +0800

    fix bug:incorrect translation

--- app/src/main/java/chinapex/com/wallet/view/me/MeManageDetailFragment.java
+++ app/src/main/java/chinapex/com/wallet/view/me/MeManageDetailFragment.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import chinapex.com.wallet.R;
 import chinapex.com.wallet.base.BaseFragment;
